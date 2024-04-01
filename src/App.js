6bc6fcbd1c2f163216841296b60a421779e26e5d
Greg Carter <gregamonintendo@gmail.com>
2024-04-01 18:07:25 +0100

    Added images and flexbox to box container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import "./App.css";
 import Logo from "./components/Logo/Logo";
 import Card from "./components/Card/Card";
+import pinkBlobImage from "./images/Pink Blob.svg";
+import designingImage from "./images/Designing.svg";
 
 function App() {
   return (
@@ -11,9 +13,23 @@ function App() {
       </div>
       <div className="body">
         <Card>
-          <h1>Under Construction</h1>
-          <p>We are currently working hard on making an amazing website.</p>
-          <p>Stay tuned...</p>
+          <div className="cardLayout">
+            <img
+              src={designingImage}
+              alt="Two cartoon people discussing work over a computer"
+              className="designingImage"
+            />
+            <div>
+              <h1>Under Construction</h1>
+              <p>We are currently working hard on making an amazing website.</p>
+              <p>Stay tuned...</p>
+              <img
+                src={pinkBlobImage}
+                alt="A Pink circle with a happy face with its arms in the air"
+                className="pinkBlob"
+              />
+            </div>
+          </div>
         </Card>
       </div>
     </>
