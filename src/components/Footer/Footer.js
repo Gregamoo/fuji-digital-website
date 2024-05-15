4256159216b57c688bbe13bd2487915c8ed5bbe6
Greg Carter <gregamonintendo@gmail.com>
2024-05-15 12:58:03 +0100

    footer files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./Footer.css";
+import Logo from "../Logo/Logo";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <Logo version={"purple"} />
+    </div>
+  );
+}
+
+export default Footer;
